da00f9402660633df6d4a27cb49091600f611b24
wangbaishun <863548227@qq.com>
2023-12-07 11:38:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CesiumContainer from "./components/CesiumContainer.vue";
+</script>
+
+<template>
+  <CesiumContainer />
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import cesium from "vite-plugin-cesium";
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-import.d.ts",
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      dts: "src/components.d.ts",
+    }),
+    cesium(),
+  ],
+});
